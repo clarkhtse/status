d9a4bf167e83ed90d5dbe8511a641996934cafbe
Tuo <clarkhtse@gmail.com>
2015-06-23 18:01:37 -0300

    remove

--- production/hotfix_1.0.js
+++
@@ -1,9 +0,0 @@
-require('HTMLDocument')
-defineClass('UDHtmlParser', {}, {
-  contentNode: function(htmlString) {
-    var document = HTMLDocument.documentWithString(htmlString)	
-    var containerNode = document.firstNodeMatchingSelector('#outer .row .small-12.large-8.columns')
-    var contentNode = containerNode.firstNodeMatchingSelector('#content .jscroll-inner')
-    return contentNode
-  }
-})
