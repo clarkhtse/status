a48d8d4fef85ae0ec1b4779dacc3cb33e345e81d
Tuo <clarkhtse@gmail.com>
2015-06-23 23:08:10 -0300

    update

--- test/hotfix_1.0.js
+++ test/hotfix_1.0.js
@@ -3,7 +3,8 @@ defineClass('UDHtmlParser', {}, {
   contentNode: function(htmlString) {
     var document = HTMLDocument.documentWithString(htmlString)	
     var containerNode = document.firstNodeMatchingSelector('#outer .row .small-12.large-8.columns')
-    if(containerNode){	    
+    if(containerNode){
+    	log('----------this is a test------------')	    
     	return containerNode.firstNodeMatchingSelector('#content .jscroll-inner')
     }
   }
