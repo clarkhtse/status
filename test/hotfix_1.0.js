505fa1696f745aea04080d40ac6646128cffb750
Tuo <clarkhtse@gmail.com>
2015-06-23 23:00:20 -0300

    spike

--- test/hotfix_1.0.js
+++ test/hotfix_1.0.js
@@ -3,10 +3,9 @@ defineClass('UDHtmlParser', {}, {
   contentNode: function(htmlString) {
     var document = HTMLDocument.documentWithString(htmlString)	
     var containerNode = document.firstNodeMatchingSelector('#outer .row .small-12.large-8.columns')
-    if(containerNode){
-	    var contentNode = containerNode.firstNodeMatchingSelector('#content .jscroll-inner')
-    	return contentNode	
+    if(containerNode){	    
+    	return containerNode.firstNodeMatchingSelector('#content .jscroll-inner')
     }
-    retur null
+    retur containerNode
   }
 })
