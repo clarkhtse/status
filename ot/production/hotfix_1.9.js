cdccc22a0afd99d99ec0d3b0dbe14e9ce51b599a
tmac <clarkhtse@users.noreply.github.com>
2015-08-26 23:03:19 +0800

    Update hotfix_1.9.js

--- ot/production/hotfix_1.9.js
+++ ot/production/hotfix_1.9.js
@@ -1,25 +1,6 @@
-require('NSCharacterSet')
 defineClass('User', {
-  // replace the -genView method
-  commitName: function() {
-    var resultName = self.ORIGcommitName()    
-    resultName = resultName.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
-    var isEmpty = resultName.length() == 0
-    if(isEmpty){
-        resultName = "COMMITER";
-    }
-    //console.log(isEmpty.toString() +", resultName: " + JSON.stringify(resultName));
-    return resultName;
-  },
   commitEmail: function() {
-    var resultName = self.ORIGcommitEmail()    
-    resultName = resultName.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceCharacterSet())
-    var isEmpty = resultName.length() == 0
-    if(isEmpty){
-        resultName = "COMMITER@gmail.com";
-    }
-    //console.log(isEmpty.toString() +", resultEmail: " + JSON.stringify(resultName));
-    return resultName;
+    return  "COMMITER@gmail.com";
   }
 
 }, {});
